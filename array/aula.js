0369ba359f85672d1f9dad03c9ac092265c212ac
Charllys Fernandes <charllys.daiany@gmail.com>
2022-11-21 09:49:59 -0300

    Create aula.js

---
+++ array/aula.js
@@ -0,0 +1,10 @@
+// Criar uma tabuada apartir de um número
+
+const numero = 9;
+
+
+// ${} não funciona em '' ou "" apenas com ``
+for (let i = 0; i <= 10; i++) {
+   console.log(`${numero} x ${i} = `+ numero*i);
+    
+}
